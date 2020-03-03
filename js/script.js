cf7efb4698a19ed0e70a304e6e21f03f7b242071
ucef8233 <-u>
2020-03-03 21:09:07 +0100

    add main
    add responsiv header

--- js/script.js
+++ js/script.js
@@ -1,8 +1,11 @@
 function myFunction() {
   var x = document.getElementById("myNav");
+  // var y = document.getElementById("Nav");
   if (x.className === "navbar__main") {
     x.className += " respNav ";
+    // y.className += " test ";
   } else {
     x.className = "navbar__main";
+    // y.classList.remove("test");
   }
 }

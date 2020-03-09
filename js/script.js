5dce736b95f60ae03abbb9be0f55448b7cba12c6
ucef8233 <-u>
2020-03-09 19:53:12 +0100

    footer ok + reglage size desktop

--- js/script.js
+++ js/script.js
@@ -1,11 +1,9 @@
 function myFunction() {
   var x = document.getElementById("myNav");
-  // var y = document.getElementById("Nav");
+
   if (x.className === "navbar__main") {
-    x.className += " respNav ";
-    // y.className += " test ";
+    x.className += " respNav";
   } else {
     x.className = "navbar__main";
-    // y.classList.remove("test");
   }
 }
